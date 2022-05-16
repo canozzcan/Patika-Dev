ffaea19e73941aa88a26e050cb51fe463c08d8e8
canozzcan <canozcanstu@gmail.com>
2022-05-16 20:13:17 +0300

    Circle Of Area

---
+++ CircleArea/findCircleArea.js
@@ -0,0 +1,8 @@
+const arguments = process.argv.slice(2);
+
+function findCircleArea(radius) {
+    const Area = Math.PI * Math.pow(radius, 2);
+    console.log(`Area of circle which is ${radius}: ${Area}`);
+}
+
+findCircleArea(arguments[0]);
