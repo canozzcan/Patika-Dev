1ad25fe6ff9431d554a7bcd9166510be64fd28d6
canozzcan <canozcanstu@gmail.com>
2022-05-16 20:18:12 +0300

    Set console.log

--- CircleArea/findCircleArea.js
+++ CircleArea/findCircleArea.js
@@ -2,7 +2,7 @@ const arguments = process.argv.slice(2);
 
 function findCircleArea(radius) {
     const Area = Math.PI * Math.pow(radius, 2);
-    console.log(`Area of circle which is ${radius}: ${Area}`);
+    console.log(`Area of circle which is radius ${radius}: ${Area}`);
 }
 
 findCircleArea(arguments[0]);
