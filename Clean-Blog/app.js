72285fdd6095907b4e4c9ad5059ba35328126c0e
canozzcan <canozcanstu@gmail.com>
2022-05-27 15:01:50 +0300

    Init Clean Blog

---
+++ Clean-Blog/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const blog = { 
+        id: 1, 
+        title: "Blog title", 
+        description: "Blog description " 
+    }
+    res.send(blog);
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started at port: ${port}`);
+});
