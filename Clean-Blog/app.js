06752e8e15fa9521d3ee086475154c0951093508
canozzcan <canozcanstu@gmail.com>
2022-05-28 16:21:41 +0300

    PatikaDev Clean Blog Task 2

--- Clean-Blog/app.js
+++ Clean-Blog/app.js
@@ -4,14 +4,26 @@ const port = 3000;
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = { 
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description " 
-    }
-    res.send(blog);
+    res.render('index');
 });
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+
 
 app.listen(port, ()=> {
     console.log(`Server started at port: ${port}`);
