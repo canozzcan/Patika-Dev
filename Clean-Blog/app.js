008802708a61709d304abf85c6e28f7bfc8c5911
canozzcan <canozcanstu@gmail.com>
2022-05-29 16:58:20 +0300

    PatikaDev Clean Blog Task 4

--- Clean-Blog/app.js
+++ Clean-Blog/app.js
@@ -39,8 +39,11 @@ app.post('/posts', async (req, res) => {
     res.redirect('/');
 });
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post', {
+        post
+    });
 });
 
 app.listen(port, () => {
