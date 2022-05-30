71241550f9cc7a77595ff66fe62bb60cbc8d143c
canozzcan <canozcanstu@gmail.com>
2022-05-30 19:47:58 +0300

    PatikaDev Clean Blog Task 5

--- Clean-Blog/app.js
+++ Clean-Blog/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Post = require('./models/Post');
+const methodOverride = require('method-override');
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageController')
 
 
 const port = 3000;
@@ -16,35 +18,22 @@ mongoose.connect('mongodb://localhost/cleanblog-test-db', {
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
-app.get('/', async (req, res) => {
-    const posts = await Post.find({});
-    res.render('index', {
-        posts: posts
-    });
-});
+app.get('/', postController.getAllPosts);
+app.post('/posts', postController.createPost);
+app.get('/posts/:id', postController.getPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
-app.get('/add_post', (req, res) => {
-    res.render('add_post');
-});
-
-app.post('/posts', async (req, res) => {
-    await Post.create(req.body)
-    res.redirect('/');
-});
-
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render('post', {
-        post
-    });
-});
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
